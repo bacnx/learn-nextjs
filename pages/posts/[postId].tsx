5b1d20542f3abb981a719f9c7e3e2497b2c20a46
Bac Nguyen <nxbac.dev@gmail.com>
2023-05-18 17:52:27 +0700

    02-07: SSG getStaticPaths with typescript

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,6 +1,49 @@
-import { useRouter } from "next/router"
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 
-export default function Post() {
-  const router = useRouter()
+interface PostProps {
+  post: any
+}
 
-  return <h2>Post id: {router.query.postId}</h2> }
+export default function Post({ post }: PostProps) {
+  if (!post) return null
+
+  return (
+    <>
+      <h1>Post detail page</h1>
+
+      <p>Post id: {post.id}</p>
+      <p>Post title: {post.title}</p>
+    </>
+  )
+}
+
+export const getStaticPaths = async () => {
+  console.log('getStaticPaths')
+
+  const response = await fetch(
+    'https://js-post-api.herokuapp.com/api/posts?_page=1'
+  )
+  const data = await response.json()
+
+  return {
+    paths: data.data.map((post: any) => ({ params: { postId: post.id } })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<PostProps> = async (
+  context: GetStaticPropsContext
+) => {
+  console.log('getStaticProps: ', context.params?.postId)
+  const postId = context.params?.postId
+  if (!postId) return { notFound: true }
+
+  const response = await fetch(
+    `https://js-post-api.herokuapp.com/api/posts/${postId}`
+  )
+  const data = await response.json()
+
+  return {
+    props: { post: { id: data.id, title: data.title } },
+  }
+}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,23 +1,46 @@
+import { GetStaticProps } from 'next'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
-export default function Post() {
-  const router = useRouter()
+interface Post {
+  id: number
+  title: string
+}
 
-  console.log(router.query)
+interface PostProps {
+  posts: Post[]
+}
 
+export default function Post({ posts }: PostProps) {
   return (
     <>
       <h1>Post Page</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
 
-export async function getServerSideProps() {
+export const getStaticProps: GetStaticProps<PostProps> = async () => {
+  const response = await fetch(
+    'https://js-post-api.herokuapp.com/api/posts?_page=1'
+  )
+  const data = await response.json()
+
   return {
-    props: {},
+    props: {
+      posts: data.data.map((x: any) => ({
+        id: x.id,
+        title: x.title,
+      })),
+    },
   }
 }
